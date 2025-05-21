83e6bc5901fcdfc36e0a512a6f83c2991965ec89
SamueleFacenda <samuele.facenda@gmail.com>
2025-05-21 14:50:51 +0200

    Init nix
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,99 @@
+# nix comments
+{
+  description = "Example nix ros project";
+
+  inputs = {
+    nix-ros-overlay.url = "github:lopsided98/nix-ros-overlay/master";
+    nixpkgs.follows = "nix-ros-overlay/nixpkgs";
+  };
+
+  nixConfig = {
+    extra-substituters = [ "https://ros.cachix.org" ];
+    extra-trusted-public-keys = [ "ros.cachix.org-1:dSyZxI8geDCJrwgvCOHDoAfOm5sV1wCPjBkKL+38Rvo=" ];
+  };
+
+  outputs = { self, nixpkgs, nix-ros-overlay }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+        overlays = [
+          nix-ros-overlay.overlays.default
+        ];
+      };
+    in {
+      packages.${system}.hba = pkgs.rosPackages.noetic.callPackage ({
+          buildRosPackage
+        , catkin
+        , roscpp
+        , rospy
+        , std-msgs
+        , geometry-msgs
+        , visualization-msgs
+        , nav-msgs
+        , sensor-msgs
+        , rosbag
+        , message-generation
+        , tf
+        , pcl
+        , eigen
+        , gtsam
+        , message-runtime
+        , pcl-conversions
+        }: 
+        buildRosPackage {
+          pname = "hba";
+          version = "1.0.1";
+          src = ./.;
+          
+          buildType = "catkin";
+          nativeBuildInputs = [ catkin ];
+          buildInputs = [
+            pcl
+            eigen
+          ];
+          propagatedBuildInputs = [
+            roscpp
+            rospy
+            std-msgs
+            geometry-msgs
+            visualization-msgs
+            nav-msgs
+            sensor-msgs
+            rosbag
+            message-generation
+            tf
+            message-runtime
+            gtsam
+            pcl-conversions
+          ];
+        }
+      ) {};
+    
+      devShells.${system} = {
+        default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.glibcLocales
+            (pkgs.rosPackages.noetic.buildEnv {
+              paths = with pkgs.rosPackages.noetic; [
+                rosbash
+                catkin
+                rqt-graph
+                teleop-twist-keyboard
+                rqt-top
+                rqt-reconfigure
+                rqt-console
+                rqt-logger-level
+                xacro
+              ];
+            })
+          ];
+
+          ROS_HOSTNAME = "localhost";
+          ROS_MASTER_URI = "http://localhost:11311";
+          QT_QPA_PLATFORM = "xcb"; # qt on wayland
+        };
+      };
+    };
+}
